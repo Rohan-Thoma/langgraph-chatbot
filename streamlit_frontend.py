8dad0bfd466c3960fd6dee3c2436ecac66f7137b
Rohan-Thoma <rohanvailalathoma@gmail.com>
2026-05-25 15:34:18 +0000

    Functioning chatbot with actual backend and history till the user refreshes the page is implemented

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
 
 #User message history should be in the session state as long as the browser session is active
 #now this session state is a dictionary as we can save anything in it with the key name of our choice.
@@ -11,6 +12,8 @@ for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
+#Since we are also maintaining history in the backend, we need to create a config and pass it
+config = {'configurable':{'thread_id': 1}}
 
 user_input = st.chat_input('Type here')
 
@@ -21,7 +24,10 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
+    #Now get the replies from the ai chatbot 
+    response = chatbot.invoke({'messages': [HumanMessage(content= user_input)]}, config=config)
+    ai_message = response['messages'][-1].content
     #Now add the assitant message also to the message history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': user_input})
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
-        st.text(user_input)
+        st.text(ai_message)
